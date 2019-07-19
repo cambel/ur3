1e128ed9e7cd84cef35685069200a982e8eb6624
Unknown <nanaki07@gmail.com>
2019-07-19 16:23:40 +0900

    control of position/rotation with keyboard

--- ur_control/scripts/joint_position_keyboard.py
+++ ur_control/scripts/joint_position_keyboard.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """
 UR Joint Position Example: keyboard
 """
@@ -9,23 +8,31 @@ import rospy
 
 from ur_control.arm import Arm
 from ur_control.constants import ROBOT_GAZEBO, ROBOT_UR_MODERN_DRIVER, ROBOT_UR_RTDE_DRIVER
+import ur_control.conversions as conversions
+
 import ur3_kinematics.e_arm as ur3_arm
 import getch
 
 import numpy as np
+from pyquaternion import Quaternion
+
+import tf
+from geometry_msgs.msg import Vector3
+np.set_printoptions(suppress=True)
 
 
-def solve_ik(arm, pose):
+def solve_ik(pose):
     """ Solve IK for ur3 arm 
         pose: [x y z aw ax ay az] array
     """
     pose = np.array(pose).reshape(1, -1)
     current_q = arm.joint_angles()
-    
+
     ik = ur3_arm.inverse(pose)
     q = best_ik_sol(ik, current_q)
-    
+
     return current_q if q is None else q
+
 
 def best_ik_sol(sols, q_guess, weights=np.ones(6)):
     """ Get best IK solution """
@@ -35,9 +42,8 @@ def best_ik_sol(sols, q_guess, weights=np.ones(6)):
         for i in range(6):
             for add_ang in [-2. * np.pi, 0, 2. * np.pi]:
                 test_ang = sol[i] + add_ang
-                if (abs(test_ang) <= 2. * np.pi
-                        and abs(test_ang - q_guess[i]) <
-                        abs(test_sol[i] - q_guess[i])):
+                if (abs(test_ang) <= 2. * np.pi and abs(test_ang - q_guess[i])
+                        < abs(test_sol[i] - q_guess[i])):
                     test_sol[i] = test_ang
         if np.all(test_sol != 9999.):
             valid_sols.append(test_sol)
@@ -48,19 +54,19 @@ def best_ik_sol(sols, q_guess, weights=np.ones(6)):
         np.sum((weights * (valid_sols - np.array(q_guess)))**2, 1))
     return valid_sols[best_sol_ind]
 
-def map_keyboard(arm):
-    arm = arm
 
+def map_keyboard():
     def print_robot_state():
-        print("Joint angles:", np.round(arm.joint_angles(), 4))
-        print("End Effector:", np.round(arm.end_effector(), 4))
+        print("Joint angles:", np.round(arm.joint_angles(), 5))
+        print("End Effector:", np.round(arm.end_effector(rot_type='euler'), 5))
+        print("quaternion:", np.round(arm.end_effector()[3:], 6))
 
     def set_j(joint_name, sign):
         global delta_q
         current_position = arm.joint_angles()
-        current_position[joint_name] += delta_q*sign
+        current_position[joint_name] += delta_q * sign
         arm.set_joint_positions_flex(current_position, t=0.25)
-        
+
     def update_d(delta, increment):
         if delta == 'q':
             global delta_q
@@ -70,22 +76,45 @@ def map_keyboard(arm):
             global delta_x
             delta_x += increment
             print "delta_x", delta_x
-        
-    def set_xyz(dim, sign):
+
+    def set_pose(dim, sign):
         global delta_x
-        x = arm.end_effector()
-        x[dim] += delta_x*sign
-        q = solve_ik(arm, x)
-        arm.set_joint_positions_flex(q, t=0.25)
+
+        J = np.zeros((6, 6))
+        twist = np.zeros(6)
+
+        x = arm.end_effector(rot_type='euler')
+        qc = arm.joint_angles()
+
+        if dim <= 2:
+            twist[dim] += delta_x * sign
+
+        if dim > 2:
+            euler = x[3:]
+            euler_diff = np.zeros(3)
+            euler_diff[dim - 3] += delta_x * sign
+
+            T = conversions.euler_transformation_matrix(euler)
+            twist[3:] = np.dot(T, euler_diff)
+
+        J = arm.kinematics.jacobian(arm.joint_angles())
+        J_inv = np.linalg.pinv(J)
+        dqc = np.dot(J_inv, twist).tolist()[0]
+
+        qc += dqc
+
+        # Publish command
+        arm.set_joint_positions_flex(position=qc, t=0.25)
+        rospy.sleep(0.25)
 
     global delta_q
     global delta_x
     delta_q = 0.0025
     delta_x = 0.0025
-    
+
     bindings = {
-    #'shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint'
-    #   key: (function, args, description)
+        #'shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint'
+        #   key: (function, args, description)
         'z': (set_j, [0, 1], "shoulder_pan_joint increase"),
         'v': (set_j, [0, -1], "shoulder_pan_joint decrease"),
         'x': (set_j, [1, 1], "shoulder_lift_joint increase"),
@@ -100,18 +129,25 @@ def map_keyboard(arm):
         'e': (set_j, [5, -1], "wrist_3_joint decrease"),
         'p': (print_robot_state, [], "right: printing"),
         # Task Space
-        'h': (set_xyz, [0, 1], "x increase"),
-        'k': (set_xyz, [0, -1], "x decrease"),
-        'y': (set_xyz, [1, 1], "y increase"),
-        'i': (set_xyz, [1, -1], "y decrease"),
-        'u': (set_xyz, [2, 1], "z increase"),
-        'j': (set_xyz, [2, -1], "z decrease"),
+        'h': (set_pose, [0, 1], "x increase"),
+        'k': (set_pose, [0, -1], "x decrease"),
+        'y': (set_pose, [1, 1], "y increase"),
+        'i': (set_pose, [1, -1], "y decrease"),
+        'u': (set_pose, [2, 1], "z increase"),
+        'j': (set_pose, [2, -1], "z decrease"),
+        'n': (set_pose, [3, 1], "ax increase"),
+        'm': (set_pose, [3, -1], "ax decrease"),
+        ',': (set_pose, [4, 1], "ay increase"),
+        '.': (set_pose, [4, -1], "ay decrease"),
+        'o': (set_pose, [5, 1], "az increase"),
+        'l': (set_pose, [5, -1], "az decrease"),
+
         # Increase or decrease delta
         '1': (update_d, ['q', 0.001], "delta_q increase"),
         '2': (update_d, ['q', -0.001], "delta_q decrease"),
         '6': (update_d, ['x', 0.001], "delta_x increase"),
         '7': (update_d, ['x', -0.001], "delta_x decrease"),
-     }
+    }
     done = False
     print("Controlling joints. Press ? for help, Esc to quit.")
     while not done and not rospy.is_shutdown():
@@ -125,13 +161,13 @@ def map_keyboard(arm):
                 cmd = bindings[c]
                 #expand binding to something like "set_j(right, 's0', 0.1)"
                 cmd[0](*cmd[1])
-                print("command: %s" % (cmd[2],))
+                print("command: %s" % (cmd[2], ))
             else:
                 print("key bindings: ")
                 print("  Esc: Quit")
                 print("  ?: Help")
-                for key, val in sorted(bindings.items(),
-                                       key=lambda x: x[1][2]):
+                for key, val in sorted(
+                        bindings.items(), key=lambda x: x[1][2]):
                     print("  %s: %s" % (key, val[2]))
 
 
@@ -149,25 +185,26 @@ def main():
 See help inside the example with the '?' key for key bindings.
     """
     arg_fmt = argparse.RawDescriptionHelpFormatter
-    parser = argparse.ArgumentParser(formatter_class=arg_fmt,
-                                     description=main.__doc__,
-                                     epilog=epilog)
-    parser.add_argument('--robot', action='store_true', help='for the real robot')
-    parser.add_argument('--beta', action='store_true', help='for the real robot. beta driver')
+    parser = argparse.ArgumentParser(
+        formatter_class=arg_fmt, description=main.__doc__, epilog=epilog)
+    parser.add_argument(
+        '--robot', action='store_true', help='for the real robot')
+    parser.add_argument(
+        '--beta', action='store_true', help='for the real robot. beta driver')
     args = parser.parse_args(rospy.myargv()[1:])
 
     rospy.init_node("joint_position_keyboard")
-    
+
     driver = ROBOT_GAZEBO
     if args.robot:
         driver = ROBOT_UR_MODERN_DRIVER
     elif args.beta:
         driver = ROBOT_UR_RTDE_DRIVER
-    
+
     global arm
-    arm = Arm(ft_sensor=False, robot=driver)
-    
-    map_keyboard(arm)
+    arm = Arm(ft_sensor=False, driver=driver)
+
+    map_keyboard()
     print("Done.")
 
 
--- ur_control/src/ur_control/conversions.py
+++ ur_control/src/ur_control/conversions.py
@@ -5,14 +5,14 @@ import numpy as np
 import tf.transformations as tr
 # Messages
 from geometry_msgs.msg import (Point, Quaternion, Pose, Vector3, Transform,
-                                                                        Wrench)
+                               Wrench)
 from sensor_msgs.msg import CameraInfo, Image, RegionOfInterest
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
-
+import pyquaternion
 
 # OpenRAVE types <--> Numpy types
 def from_dict(transform_dict):
-  """
+    """
   Converts a dictionary with the fields C{rotation} and C{translation}
   into a homogeneous transformation of type C{np.array}.
   @type transform_dict:  dict
@@ -20,151 +20,163 @@ def from_dict(transform_dict):
   @rtype: np.array
   @return: The resulting numpy array
   """
-  T = tr.quaternion_matrix(np.array(transform_dict['rotation']))
-  T[:3,3] = np.array(transform_dict['translation'])
-  return T
+    T = tr.quaternion_matrix(np.array(transform_dict['rotation']))
+    T[:3, 3] = np.array(transform_dict['translation'])
+    return T
+
 
 # PyKDL types <--> Numpy types
 def from_kdl_vector(vector):
-  """
+    """
   Converts a C{PyKDL.Vector} with fields into a numpy array.
   @type  vector: PyKDL.Vector
   @param vector: The C{PyKDL.Vector} to be converted
   @rtype: np.array
   @return: The resulting numpy array
   """
-  return np.array([vector.x(),vector.y(),vector.z()])
+    return np.array([vector.x(), vector.y(), vector.z()])
+
 
 def from_kdl_twist(twist):
-  """
+    """
   Converts a C{PyKDL.Twist} with fields into a numpy array.
   @type  twist: PyKDL.Twist
   @param twist: The C{PyKDL.Twist} to be converted
   @rtype: np.array
   @return: The resulting numpy array
   """
-  array = np.zeros(6)
-  array[:3] = from_kdl_vector(twist.vel)
-  array[3:] = from_kdl_vector(twist.rot)
-  return array
+    array = np.zeros(6)
+    array[:3] = from_kdl_vector(twist.vel)
+    array[3:] = from_kdl_vector(twist.rot)
+    return array
 
 
 # ROS types <--> Numpy types
 def from_point(msg):
-  """
+    """
   Converts a C{geometry_msgs/Point} ROS message into a numpy array.
   @type  msg: geometry_msgs/Point
   @param msg: The ROS message to be converted
   @rtype: np.array
   @return: The resulting numpy array
   """
-  return from_vector3(msg)
+    return from_vector3(msg)
+
 
 def from_pose(msg):
-  """
+    """
   Converts a C{geometry_msgs/Pose} ROS message into a numpy array (Homogeneous transformation 4x4).
   @type  msg: geometry_msgs/Pose
   @param msg: The ROS message to be converted
   @rtype: np.array
   @return: The resulting numpy array
   """
-  T = tr.quaternion_matrix(from_quaternion(msg.orientation))
-  T[:3,3] = from_point(msg.position)
-  return T
+    T = tr.quaternion_matrix(from_quaternion(msg.orientation))
+    T[:3, 3] = from_point(msg.position)
+    return T
+
 
 def from_quaternion(msg):
-  """
+    """
   Converts a C{geometry_msgs/Quaternion} ROS message into a numpy array.
   @type  msg: geometry_msgs/Quaternion
   @param msg: The ROS message to be converted
   @rtype: np.array
   @return: The resulting numpy array
   """
-  return np.array([msg.x, msg.y, msg.z, msg.w])
+    return np.array([msg.x, msg.y, msg.z, msg.w])
+
 
 def from_roi(msg):
-  top_left = np.array([msg.x_offset, msg.y_offset])
-  bottom_right = top_left + np.array([msg.width, msg.height])
-  return [top_left, bottom_right]
+    top_left = np.array([msg.x_offset, msg.y_offset])
+    bottom_right = top_left + np.array([msg.width, msg.height])
+    return [top_left, bottom_right]
+
 
 def from_transform(msg):
-  """
+    """
   Converts a C{geometry_msgs/Transform} ROS message into a numpy array.
   @type  msg: geometry_msgs/Transform
   @param msg: The ROS message to be converted
   @rtype: np.array
   @return: The resulting numpy array
   """
-  T = tr.quaternion_matrix(from_quaternion(msg.rotation))
-  T[:3,3] = from_vector3(msg.translation)
-  return T
+    T = tr.quaternion_matrix(from_quaternion(msg.rotation))
+    T[:3, 3] = from_vector3(msg.translation)
+    return T
+
 
 def from_vector3(msg):
-  """
+    """
   Converts a C{geometry_msgs/Vector3} ROS message into a numpy array.
   @type  msg: geometry_msgs/Vector3
   @param msg: The ROS message to be converted
   @rtype: np.array
   @return: The resulting numpy array
   """
-  return np.array([msg.x, msg.y, msg.z])
+    return np.array([msg.x, msg.y, msg.z])
+
 
 def from_wrench(msg):
-  """
+    """
   Converts a C{geometry_msgs/Wrench} ROS message into a numpy array.
   @type  msg: geometry_msgs/Wrench
   @param msg: The ROS message to be converted
   @rtype: np.array
   @return: The resulting numpy array
   """
-  array = np.zeros(6)
-  array[:3] = from_vector3(msg.force)
-  array[3:] = from_vector3(msg.torque)
-  return array
+    array = np.zeros(6)
+    array[:3] = from_vector3(msg.force)
+    array[3:] = from_vector3(msg.torque)
+    return array
+
 
 def to_quaternion(array):
-  """
+    """
   Converts a numpy array into a C{geometry_msgs/Quaternion} ROS message.
   @type  array: np.array
   @param array: The position as numpy array
   @rtype: geometry_msgs/Quaternion
   @return: The resulting ROS message
   """
-  return Quaternion(*array)
+    return Quaternion(*array)
+
 
 def to_point(array):
-  """
+    """
   Converts a numpy array into a C{geometry_msgs/Point} ROS message.
   @type  array: np.array
   @param array: The position as numpy array
   @rtype: geometry_msgs/Point
   @return: The resulting ROS message
   """
-  return Point(*array)
+    return Point(*array)
 
 
 def to_pose(T):
-  """
+    """
   Converts a homogeneous transformation (4x4) into a C{geometry_msgs/Pose} ROS message.
   @type  T: np.array
   @param T: The homogeneous transformation
   @rtype: geometry_msgs/Pose
   @return: The resulting ROS message
   """
-  pos = Point(*T[:3,3])
-  quat = Quaternion(*tr.quaternion_from_matrix(T))
-  return Pose(pos, quat)
+    pos = Point(*T[:3, 3])
+    quat = Quaternion(*tr.quaternion_from_matrix(T))
+    return Pose(pos, quat)
+
 
 def to_roi(top_left, bottom_right):
-  msg = RegionOfInterest()
-  msg.x_offset = round(top_left[0])
-  msg.y_offset = round(top_left[1])
-  msg.width = round(abs(bottom_right[0]-top_left[0]))
-  msg.height = round(abs(bottom_right[1]-top_left[1]))
-  return msg
+    msg = RegionOfInterest()
+    msg.x_offset = round(top_left[0])
+    msg.y_offset = round(top_left[1])
+    msg.width = round(abs(bottom_right[0] - top_left[0]))
+    msg.height = round(abs(bottom_right[1] - top_left[1]))
+    return msg
+
 
 def to_transform(T):
-  """
+    """
   Converts a homogeneous transformation (4x4) into a C{geometry_msgs/Transform}
   ROS message.
   @type  T: np.array
@@ -172,38 +184,39 @@ def to_transform(T):
   @rtype: geometry_msgs/Transform
   @return: The resulting ROS message
   """
-  translation = Vector3(*T[:3,3])
-  rotation = Quaternion(*tr.quaternion_from_matrix(T))
-  return Transform(translation, rotation)
+    translation = Vector3(*T[:3, 3])
+    rotation = Quaternion(*tr.quaternion_from_matrix(T))
+    return Transform(translation, rotation)
+
 
 def to_vector3(array):
-  """
+    """
   Converts a numpy array into a C{geometry_msgs/Vector3} ROS message.
   @type  array: np.array
   @param array: The vector as numpy array
   @rtype: geometry_msgs/Vector3
   @return: The resulting ROS message
   """
-  return Vector3(*array)
+    return Vector3(*array)
 
 
 def to_wrench(array):
-  """
+    """
   Converts a numpy array into a C{geometry_msgs/Wrench} ROS message.
   @type  array: np.array
   @param array: The wrench as numpy array
   @rtype: geometry_msgs/Wrench
   @return: The resulting ROS message
   """
-  msg = Wrench()
-  msg.force = to_vector3(array[:3])
-  msg.torque = to_vector3(array[3:])
-  return msg
+    msg = Wrench()
+    msg.force = to_vector3(array[:3])
+    msg.torque = to_vector3(array[3:])
+    return msg
 
 
 # RViz types <--> Numpy types
 def from_rviz_vector(value, dtype=float):
-  """
+    """
   Converts a RViz property vector in the form C{X;Y;Z} into a numpy array.
   @type  value: str
   @param value: The RViz property vector
@@ -212,5 +225,66 @@ def from_rviz_vector(value, dtype=float):
   @rtype: array
   @return: The resulting numpy array
   """
-  strlst = value.split(';')
-  return np.array( map(dtype, strlst) )
+    strlst = value.split(';')
+    return np.array(map(dtype, strlst))
+
+
+def angleAxis_from_euler(euler):
+    roll, pitch, yaw = euler
+    yaw_matrix = np.matrix([[np.cos(yaw), -np.sin(yaw), 0],
+                            [np.sin(yaw), np.cos(yaw), 0], [0, 0, 1]])
+
+    pitch_matrix = np.matrix([[np.cos(pitch), 0,
+                               np.sin(pitch)], [0, 1, 0],
+                              [-np.sin(pitch), 0,
+                               np.cos(pitch)]])
+
+    roll_matrix = np.matrix([[1, 0, 0], [0, np.cos(roll), -np.sin(roll)],
+                             [0, np.sin(roll), np.cos(roll)]])
+
+    R = yaw_matrix * pitch_matrix * roll_matrix
+
+    theta = np.arccos(((R[0, 0] + R[1, 1] + R[2, 2]) - 1) / 2)
+    if theta == 0:
+        return [0., 0., 0.]
+
+    multi = 1 / (2 * np.sin(theta))
+
+    rx = multi * (R[2, 1] - R[1, 2]) * theta
+    ry = multi * (R[0, 2] - R[2, 0]) * theta
+    rz = multi * (R[1, 0] - R[0, 1]) * theta
+    return [rx, ry, rz]
+
+
+def euler_transformation_matrix(euler):
+    """ Euler's transformation matrix """
+    r, p, y = euler
+    T = np.array([[1, 0, np.sin(p)], [0, np.cos(r), -np.sin(r) * np.cos(p)],
+                  [0, np.sin(r), np.cos(r) * np.cos(p)]])
+    return T
+
+def transform_end_effector(pose, extra_pose, matrix=False):
+    """
+    Transform end effector pose
+      pose: current pose [x, y, z, ax, ay, az, w]
+      extra_pose: additional transformation [x, y, z, ax, ay, az, w]
+      matrix: if true: return (translation, rotation matrix)
+              else: return translation + quaternion list
+    """
+    extra_translation = np.array(extra_pose[:3]).reshape(3, 1)
+    extra_rot = pyquaternion.Quaternion(np.roll(extra_pose[3:], 1)).rotation_matrix
+
+    c_trans = np.array(pose[:3]).reshape(3, 1)
+    c_rot = pyquaternion.Quaternion(np.roll(
+        pose[3:],
+        1)).rotation_matrix  # BE CAREFUL!! Pose from KDL is ax ay az aw
+    #              Pose from IKfast is aw ax ay az
+
+    n_trans = np.matmul(c_rot, extra_translation) + c_trans
+    n_rot = np.matmul(c_rot, extra_rot)
+
+    if matrix:
+        return n_trans.flatten(), n_rot
+
+    return n_trans.flatten().tolist() + np.roll(
+        pyquaternion.Quaternion(matrix=n_rot).normalised.elements, -1).tolist()
