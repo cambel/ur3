d7f311dd1e14d602cf987854d992eb69717a2a7a
Cristian Beltran <cristianbehe@gmail.com>
2023-05-25 15:11:14 +0900

    Update logger

--- ur3e_openai/src/ur3e_openai/initialize_logger.py
+++ ur3e_openai/src/ur3e_openai/initialize_logger.py
@@ -1,19 +1,46 @@
-import os
 import logging
 from logging import getLogger, StreamHandler, FileHandler, Formatter
-import datetime
 
+GREEN = 21
+logging.addLevelName(GREEN, "GREEN")
+def green(self, message, *args, **kws):
+    if self.isEnabledFor(GREEN):
+        # Yes, logger takes its '*args' as 'args'.
+        self._log(GREEN, message, args, **kws) 
+logging.Logger.green = green
+
+class CustomFormatter(logging.Formatter):
+
+    cyan = '\033[36m'
+    green = '\033[32m'
+    yellow = '\033[93m'
+    red = '\033[91m'
+    bold_red = '\033[1;31m'
+    reset = '\033[0m'
+    format = '%(levelname)s-%(filename)s:%(lineno)s %(message)s'
+
+    FORMATS = {
+        logging.DEBUG: cyan + format + reset,
+        logging.INFO: format,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset,
+        GREEN: green + format + reset,
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
 
 def initialize_logger(logging_level=logging.INFO, filename=None, save_log=True, log_tag="ur3e_openai"):
     logger = getLogger(log_tag)
     logger.setLevel(logging_level)
 
-    handler_format = Formatter(
-        '%(asctime)s.%(msecs)03d [%(levelname)s] (%(filename)s:%(lineno)s) %(message)s',
-        "%H:%M:%S")  # If you want to show date, add %Y-%m-%d
+    handler_format = Formatter('%(levelname)s-%(filename)s:%(lineno)s %(message)s')
     stream_handler = StreamHandler()
     stream_handler.setLevel(logging_level)
-    stream_handler.setFormatter(handler_format)
+    stream_handler.setFormatter(CustomFormatter())
 
     if save_log:
         if filename is not None:
